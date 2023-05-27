b00f14fa6dd49c2e8c3ef2ecd48f932c97c64bb3
Jonathan Clark <contact@refactorthis.dev>
2023-05-27 10:57:49 -0700

    fix: callback url was incorrect

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
 import { ShopifyModule } from './shopify/shopify.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   controllers: [],
   providers: [],
-  imports: [ConfigModule.forRoot({ cache: true }), DbModule, ShopifyModule]
+  imports: [
+    ConfigModule.forRoot({ cache: true }),
+    DbModule,
+    ShopifyModule,
+    PrismaModule,
+  ],
 })
-export class AppModule { }
+export class AppModule {}
--- api/src/shopify/shopify.service.ts
+++ api/src/shopify/shopify.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import '@shopify/shopify-api/adapters/node';
-import { shopifyApi, LATEST_API_VERSION, BillingInterval } from '@shopify/shopify-api';
+import {
+  shopifyApi,
+  LATEST_API_VERSION,
+  BillingInterval,
+} from '@shopify/shopify-api';
 import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 const billing = {
   'My plan': {
     amount: 5.0,
     currencyCode: 'USD',
     interval: BillingInterval.OneTime,
-  }
-}
+  },
+};
 
 
 @Injectable()
 export class ShopifyService {
-  constructor(private config: ConfigService) { }
-  shopify = shopifyApi({
+  constructor(private config: ConfigService, private db: PrismaService) { }
+  shopifyApi = shopifyApi({
     apiKey: this.config.get('SHOPIFY_API_KEY'),
     apiSecretKey: this.config.get('SHOPIFY_API_SECRET_KEY'),
-    scopes: this.config.get('SCOPES'),
+    scopes: this.config.get('SCOPES').split(','),
     hostName: this.config.get('HOSTNAME'),
     hostScheme: this.config.get('HOSTSCHEME'),
     apiVersion: LATEST_API_VERSION,
@@ -27,13 +32,15 @@ export class ShopifyService {
     userAgentPrefix: 'Custom prefix',
     // Change this to the billing object above once you are ready
     billing: undefined,
-  })
+  });
 
+  // AUTH
   async initAuth(req: any, res: Response, query: any) {
-    console.log('initAuth')
-    return await this.shopify.auth.begin({
-      shop: this.shopify.utils.sanitizeShop(req.query.shop, true),
-      callbackPath: '/shopify/auth/callback',
+    console.log('initAuth');
+    console.log({ req, res, query })
+    return await this.shopifyApi.auth.begin({
+      shop: req.query.shop,
+      callbackPath: '/',
       isOnline: false,
       rawRequest: req,
       rawResponse: res,
@@ -41,12 +48,23 @@ export class ShopifyService {
   }
 
   async authCallback(req: any, res: any) {
-    await this.shopify.auth.callback({
+    const callbackResponse = await this.shopifyApi.auth.callback({
       rawRequest: req,
       rawResponse: res,
     });
 
-    const host = this.shopify.utils.sanitizeHost(req.query.host);
-    return res.redirect(`${this.config.get('HOSTNAME')}/?shop=${req.query.shop}&host=${encodeURIComponent(host)}`);
+    await this.storeSession(callbackResponse.session.toObject());
+
+    const host = this.shopifyApi.utils.sanitizeHost(req.query.host);
+    const shop = this.shopifyApi.utils.sanitizeShop(req.query.shop, true);
+
+    const redirectUrl = this.shopifyApi.auth.buildEmbeddedAppUrl('https://appclient.thingify.dev/')
+    return res.redirect(redirectUrl);
+
+  }
+
+  // SESSION
+  async storeSession(session) {
+    console.log({ session });
   }
 }
